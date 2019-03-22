93615dac20393b7c4c9069762038cb08ac6a925e
Bedoya, Fabricio <Fabricio.Bedoya@edelman.com>
2019-03-21 22:42:34 -0400

    remove fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
